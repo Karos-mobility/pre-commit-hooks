56402db3732a0ae5a6bd272724609057aca2b608
Thomas Kgaevski <thomas.kgaevski@karos.fr>
2024-06-12 16:27:03 +0200

    Add check-import-datetime precommit hook

---
+++ pre_commit_hooks/check_import_datetime.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import argparse
+import re
+from typing import Sequence
+
+PATTERN = re.compile(r"^\s*import datetime$")
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    retcode = 0
+    for filename in args.filenames:
+        with open(filename, 'rb') as inputfile:
+            for i, line in enumerate(inputfile, start=1):
+                if PATTERN.match(line) is not None:
+                    print(
+                        f'{filename}:{i}: `import datetime` found, '
+                        'use `from datetime import ...` instead'
+                    )
+                    retcode = 1
+
+    return retcode
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
